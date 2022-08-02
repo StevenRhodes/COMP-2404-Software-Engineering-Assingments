94ecfb0d3b48291429a9bb376222f75642eab9c1
StevenRhodes <steverhodes@cmail.carleton.ca>
2022-08-02 12:09:22 -0400

    Uploading Assignments

---
+++ Assignments/Assignment 3/assignment1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STR 32
+#define MAX_VEHICLES 4
+#define MAX_CUSTOMERS 6
+
+typedef struct {
+
+	// ADD MEMBER VARIABLES
+
+} VehicleType;
+
+typedef struct {
+
+	// ADD MEMBER VARIABLES
+
+} CustomerType;
+
+void print_vehicle(VehicleType *v) {
+
+}
+
+void print_customer(CustomerType *c) {
+      
+}
+
+int add_vehicle(CustomerType *customer, VehicleType *vehicle) {
+
+}
+
+CustomerType create_customer(char* fname, char* lname) {
+
+}
+
+VehicleType create_vehicle(char* make, char* model, char* colour,
+                            int year, int mileage) {
+
+}
+
+
+int main() {
+
+    CustomerType customers[MAX_CUSTOMERS];
+    
+    return 0;
+}
+
+
